70bfb679862dbab8d4cc9273eea49b506c8a21f3
Nipun Ravisara <ravisara.dev@gmail.com>
2022-01-30 10:18:28 +0530

    Reguler updates.

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -23,8 +23,8 @@ keymap("n", "<C-Space>", ":NvimTreeToggle <CR>", opts)
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
@@ -62,3 +62,42 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
+-- Buffer Bar --
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map('n', '<A-,>', ':BufferPrevious<CR>', opts)
+map('n', '<A-.>', ':BufferNext<CR>', opts)
+-- Re-order to previous/next
+map('n', '<A-<>', ':BufferMovePrevious<CR>', opts)
+map('n', '<A->>', ' :BufferMoveNext<CR>', opts)
+-- Goto buffer in position...
+map('n', '<A-1>', ':BufferGoto 1<CR>', opts)
+map('n', '<A-2>', ':BufferGoto 2<CR>', opts)
+map('n', '<A-3>', ':BufferGoto 3<CR>', opts)
+map('n', '<A-4>', ':BufferGoto 4<CR>', opts)
+map('n', '<A-5>', ':BufferGoto 5<CR>', opts)
+map('n', '<A-6>', ':BufferGoto 6<CR>', opts)
+map('n', '<A-7>', ':BufferGoto 7<CR>', opts)
+map('n', '<A-8>', ':BufferGoto 8<CR>', opts)
+map('n', '<A-0>', ':BufferLast<CR>', opts)
+map('n', '<A-9>', ':BufferGoto 9<CR>', opts)
+-- Close buffer
+map('n', '<A-c>', ':BufferClose<CR>', opts)
+-- Wipeout buffer
+--                 :BufferWipeout<CR>
+-- Close commands
+--                 :BufferCloseAllButCurrent<CR>
+--                 :BufferCloseBuffersLeft<CR>
+--                 :BufferCloseBuffersRight<CR>
+-- Magic buffer-picking mode
+map('n', '<C-p>', ':BufferPick<CR>', opts)
+-- Sort automatically by...
+map('n', '<Space>bb', ':BufferOrderByBufferNumber<CR>', opts)
+map('n', '<Space>bd', ':BufferOrderByDirectory<CR>', opts)
+map('n', '<Space>bl', ':BufferOrderByLanguage<CR>', opts)
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -14,13 +14,24 @@ end
 
 -- Plugins
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim' -- Packer
-  use 'kyazdani42/nvim-web-devicons' -- Icons
-  use 'kyazdani42/nvim-tree.lua' -- Nvim tree
-  use 'nvim-lualine/lualine.nvim' -- Lualine
+    use 'wbthomason/packer.nvim' -- Packer
+    use 'kyazdani42/nvim-web-devicons' -- Icons
+    use 'kyazdani42/nvim-tree.lua' -- Nvim tree
+    use 'nvim-lualine/lualine.nvim' -- Lualine
+    use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    use 'EdenEast/nightfox.nvim' -- Color scheme
+    use 'rcarriga/nvim-notify' -- Notification 
+    use 'romgrk/barbar.nvim' -- Buffer bar
+    use { 'AlphaTechnolog/pywal.nvim', as = 'pywal' }
 
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+    require'lspconfig'.pyright.setup{}
+    use 'onsails/lspkind-nvim'  -- Lsp menu icons
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
 
